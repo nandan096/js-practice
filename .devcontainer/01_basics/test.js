f47e804a6ebae4118cc4f332666e65105f268d04
Nandakumar <147599351+nandan096@users.noreply.github.com>
2024-08-14 11:15:35 +0000

    Dates in js

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,8 @@
-console.log("hello")
+let mytime=new Date() //object
+console.log(mytime);                        //2024-08-14T11:12:02.290Z
+console.log(mytime.toDateString())          //Wed Aug 14 2024
+console.log(mytime.toISOString());          //2024-08-14T11:12:02.290Z
+console.log(mytime.toJSON());               //2024-08-14T11:12:02.290Z
+console.log(mytime.toLocaleDateString());   //8/14/2024
+console.log(mytime.toString());             //Wed Aug 14 2024 11:12:02 GMT+0000 (Coordinated Universal Time)
+
